232f3cb6e20c291bfe9ad3f379d009837f936407
daniel.casidevelper <56042562+casideveloper@users.noreply.github.com>
2020-03-23 18:47:36 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 $correo = $_REQUEST['emausu'];
 
-$cnx = new PDO("mysql:host=localhost;dbname=set27per_xamarin","set27per_daniel","PM205356");
+$cnx = new PDO("mysql:host=localhost;dbname=set27per_xamarin","set27per_danielvalerio","PM205356");
 $res = $cnx->query("select * from usuario where emausu='$correo'");
 
 $datos = array();
