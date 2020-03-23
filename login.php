d7763787875354abab1f2d2d489f22a8384da072
casi.developer <daniel.casideveloper@gmail.com>
2020-03-23 18:26:29 -0500

    configuracion servidor login

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 $correo = $_REQUEST['emausu'];
 
-$cnx = new PDO("mysql:host=localhost;dbname=set27per_navidad","root","123456");
+$cnx = new PDO("mysql:host=localhost;dbname=set27per_xamarin","set27per_daniel","PM205356");
 $res = $cnx->query("select * from usuario where emausu='$correo'");
 
 $datos = array();
